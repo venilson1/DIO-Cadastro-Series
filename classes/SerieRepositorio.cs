61f06672577258c57118dcc7514450875e82b1ed
venilson1 <venilson2009@hotmail.com>
2021-03-16 23:41:18 -0300

    Repositorio de Serie

---
+++ classes/SerieRepositorio.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using crud_serie.Interfaces;
+
+namespace crud_serie.classes
+{
+  public class SerieRepositorio : IRepositorio<Serie>
+  {
+    private List<Serie> listaSerie = new List<Serie>();
+    public void Atualizar(int id, Serie objeto)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void Excluir(int id)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void Inserir(Serie objeto)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public List<Serie> Lista()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public int ProximoId()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public Serie RetornaPorId(int id)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
