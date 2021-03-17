e93d052f70f0533f54c757af711f1a797a53be5a
venilson1 <venilson2009@hotmail.com>
2021-03-16 22:38:34 -0300

    Constructor Serie

--- classes/Serie.cs
+++ classes/Serie.cs
@@ -6,5 +6,13 @@ namespace crud_serie.classes
         public string Titulo { get; set; }  
         public string Descricao { get; set; }   
         public int Ano { get; set; }
+
+        public  Serie(int id, Genero genero, string titulo, string descricao, int ano) {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
     }
 }
