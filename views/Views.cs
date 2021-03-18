d8378723dc631fdb7e04c06676a0f197de960536
venilson1 <venilson2009@hotmail.com>
2021-03-18 13:23:55 -0300

    template de visualização dos métodos

---
+++ views/Views.cs
@@ -0,0 +1,11 @@
+namespace crud_serie.views
+{
+    public class Views
+    {
+        public static void ListarSerie(){ }
+        public static void InserirSerie(){ }
+        public static void AtualizarSerie(){ }
+        public static void ExcluirSerie(){ }
+        public static void VisualizarSerie(){ }
+    }
+}
